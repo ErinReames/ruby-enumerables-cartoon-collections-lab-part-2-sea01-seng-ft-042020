3ae715a021634b1329d88a69283ad8c45d5150bb
Erin Reames <italiagirl83@gmail.com>
2020-04-04 21:56:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,5 +25,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   #No map
   #Not asking for a Boolean 
-  planeteer_calls.find{|element| element.include? (valid_calls)}
+  planeteer_calls.find{|element| }
 end
